d6ccbf012f86f2890eb65d5aa1bbbc72342e5b62
motibegna <begna002@umn.edu>
2024-02-23 12:53:29 -0600

    Enabling Network

--- app/src/main/java/com/example/groupassignment2/screens/Profile1.kt
+++ app/src/main/java/com/example/groupassignment2/screens/Profile1.kt
@@ -7,19 +7,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import com.example.groupassignment2.Drawer
 import com.example.groupassignment2.MainViewModel
-import com.example.groupassignment2.NavRoutes
+import java.time.Instant
+import java.util.Date
 
 
 @Composable
 fun Profile1(navController: NavHostController, viewModel: MainViewModel) {
+    viewModel.profileString = "Moti visited on ${Date.from(Instant.now())}"
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
