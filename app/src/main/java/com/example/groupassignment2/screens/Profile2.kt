2f5cee25eaf77514f45e2f7b9b09c5aeb6225186
B7-9414 <banawi.bassam@gmail.com>
2024-02-25 11:47:27 -0600

    card-logs-inputs events and TopSection added it ..

--- app/src/main/java/com/example/groupassignment2/screens/Profile2.kt
+++ app/src/main/java/com/example/groupassignment2/screens/Profile2.kt
@@ -1,7 +1,6 @@
 package com.example.groupassignment2.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-<<<<<<< Updated upstream
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -29,35 +28,5 @@ fun Profile2(viewModel: MainViewModel) {
         Spacer(modifier = Modifier.height(10.dp))
         EventsFormSection()
     }
-=======
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import com.example.groupassignment2.CardsSection
-import com.example.groupassignment2.CurrenciesSection
-import com.example.groupassignment2.Drawer
-import com.example.groupassignment2.MainViewModel
-import com.example.groupassignment2.NavRoutes
-import com.example.groupassignment2.WalletSection
-
-
-@Composable
-fun Profile2(navController: NavHostController, viewModel: MainViewModel) {
-//    Box(
-//        modifier = Modifier.fillMaxSize(),
-//        contentAlignment = Alignment.Center
-//
-//    ) {
-//        Column (horizontalAlignment = Alignment.CenterHorizontally){
-//            Text("Bassam screen page", style = MaterialTheme.typography.headlineSmall)
-//        }
-//    }
-    CardsSection()
-    WalletSection()
-    CurrenciesSection()
-//    Drawer(navController)
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
 
