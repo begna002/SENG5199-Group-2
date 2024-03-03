48eb8e51e4306448267ddccef7225edd8e4b7c7c
esnaaf1 <farshad.esnaashari@gmail.com>
2024-03-03 17:06:54 -0600

    Updated Farshad's branch:  Added Profile4Content for the Profile4 page

--- app/src/main/java/com/example/groupassignment2/screens/Profile4.kt
+++ app/src/main/java/com/example/groupassignment2/screens/Profile4.kt
@@ -1,14 +1,10 @@
 package com.example.groupassignment2.screens
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.groupassignment2.MainScreen
 import com.example.groupassignment2.MainViewModel
 import com.example.groupassignment2.profile4Content.RestaurantsScreen
 import java.time.Instant
